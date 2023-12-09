c3246079d44f1245371a0bb25844a371a2366550
Arianna Azizi <aziziarianna@gmail.com>
2023-12-08 21:44:00 -0500

    simplified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './components/login/LoginPage';
-import MainPage from './components/main/MainPage';
 import TopicSelection from './components/main/TopicSelection';
 import QuizContainer from './components/quiz/QuizContainer';
 
@@ -31,11 +30,7 @@ const App = () => {
         <Routes>
             <Route
                 path="/login"
-                element={!isLoggedIn ? <LoginPage onLoginSuccess={handleLoginSuccess} /> : <Navigate replace to="/main" />}
-            />
-            <Route
-                path="/main"
-                element={isLoggedIn ? <MainPage onStartQuiz={handleStartQuiz} onContact={() => {}} /> : <Navigate replace to="/login" />}
+                element={!isLoggedIn ? <LoginPage onLoginSuccess={handleLoginSuccess} /> : <Navigate replace to="/topic-selection" />}
             />
             <Route
                 path="/topic-selection"
@@ -43,24 +38,11 @@ const App = () => {
             />
             <Route
                 path="/quiz"
-                element={isLoggedIn && selectedTopic ? <QuizContainer topic={selectedTopic} questions={questions} /> : <Navigate replace to="/main" />}
+                element={isLoggedIn && selectedTopic ? <QuizContainer topic={selectedTopic} questions={questions} /> : <Navigate replace to="/topic-selection" />}
             />
             <Route path="/" element={<Navigate replace to="/login" />} />
         </Routes>
     </Router>
-      /*  <Router>
-            {isLoggedIn} 
-            <Routes>
-                <Route path="/login" element={!isLoggedIn ? <LoginPage onLoginSuccess={handleLoginSuccess} /> : <Navigate replace to="/main" />} />
-                <Route path="/main" element={isLoggedIn ? <MainPage onStartQuiz={handleStartQuiz} onContact={() => {}} /> : <Navigate replace to="/login" />} />
-                <Route path="/topic-selection" element={isLoggedIn ? <TopicSelection onSelectTopic={handleSelectTopic} /> : <Navigate replace to="/login" />} />
-                <Route path="/quiz" element={isLoggedIn && selectedTopic ? <QuizContainer topic={selectedTopic} /> : <Navigate replace to="/main" />} />
-                <Route path="/quiz" element={isLoggedIn && selectedTopic ? <QuizContainer topic={selectedTopic} questions={questions} /> : <Navigate replace to="/main" />} />
-                <Route path="/" element={<Navigate replace to="/login" />} />
-
-            </Routes>
-            {isLoggedIn}
-        </Router>*/
     );
 };
 
